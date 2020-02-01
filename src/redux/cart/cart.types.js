cdad56ad6b778911b78bb63af8843ecbdb7e694d
akostikov92 <akostikov92@gmail.com>
2020-02-01 13:10:24 -0700

    updated shop data with new data, updated cart utils to group items with multiple quantity

--- src/redux/cart/cart.types.js
+++ src/redux/cart/cart.types.js
@@ -1,7 +1,8 @@
 /** @format */
 
 const CartActionTypes = {
-  TOGGLE_CART_HIDDEN: 'TOGGLE_CART_HIDDEN'
+  TOGGLE_CART_HIDDEN: 'TOGGLE_CART_HIDDEN',
+  ADD_ITEM: 'ADD_ITEM'
 };
 
 export default CartActionTypes;
